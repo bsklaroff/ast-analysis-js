ddc8a1e073472f94024831b8d4a6826993a1d24d
Ben Sklaroff <bsklaroff@gmail.com>
2014-01-21 00:53:54 -0800

    add basic autocomplete functionality

--- static/feedback.js
+++ static/feedback.js
@@ -63,9 +63,60 @@ var nodeTypes = [
 ];
 
 $(function() {
-  var editor = ace.edit('editor');
+  var programSpec, editor;
+  programSpec = {
+    whitelist: [],
+    blacklist: [],
+    structure: [],
+    nonBlacklistNodes: nodeTypes,
+    nonListNodes: nodeTypes
+  };
+  editor = ace.edit('editor');
   editor.setTheme('ace/theme/monokai');
   editor.getSession().setMode('ace/mode/javascript');
+
+  var genAddListItem = function(listName) {
+    return function(e, ui) {
+      var i, nodeType = ui.item.label;
+      e.preventDefault();
+      $('.list_input').val('');
+
+      programSpec[listName].push(nodeType);
+      if (listName === 'blacklist' || listName === 'whitelist') {
+        programSpec['nonBlacklistNodes'] = [];
+        programSpec['nonListNodes'] = [];
+        for (i = 0; i < nodeTypes.length; i++) {
+          if (programSpec['blacklist'].indexOf(nodeTypes[i]) === -1) {
+            programSpec['nonBlacklistNodes'].push(nodeTypes[i]);
+            if (programSpec['whitelist'].indexOf(nodeTypes[i]) === -1) {
+              programSpec['nonListNodes'].push(nodeTypes[i]);
+            }
+          }
+        }
+      } else {
+        // Handle structure nodes
+      }
+
+      $('#'+listName+'_list').append('<div>' + nodeType + '</div>');
+      refreshAutocompletes();
+    };
+  };
+
+  var refreshAutocompletes = function() {
+    $('#whitelist_input').autocomplete({
+      source: programSpec['nonListNodes'],
+      select: genAddListItem('whitelist')
+    });
+    $('#blacklist_input').autocomplete({
+      source: programSpec['nonListNodes'],
+      select: genAddListItem('blacklist')
+    });
+    $('#structure_input').autocomplete({
+      source: programSpec['nonBlacklistNodes'],
+      select: genAddListItem('structure')
+    });
+  };
+  refreshAutocompletes();
 
   $('#submit_code').click(function() {
     $.ajax({
