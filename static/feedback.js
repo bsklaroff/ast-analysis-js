6492d98ad60ab340e5a111f28586165135a4aaba
Ben Sklaroff <bsklaroff@gmail.com>
2014-01-21 01:56:55 -0800

    add bootstrap and add ability to unlist items

--- static/feedback.js
+++ static/feedback.js
@@ -77,56 +77,73 @@ $(function() {
   editor.setTheme('ace/theme/monokai');
   editor.getSession().setMode('ace/mode/javascript');
 
+  var computeStructure = function() {
+    /*
+    if (programSpec['structureList'].indexOf(nodeType) === -1) {
+      programSpec['structureList'].push(nodeType);
+    }*/
+  };
+
   var genAddListItem = function(listName) {
     return function(e, ui) {
       var i, nodeType = ui.item.label;
       e.preventDefault();
       $('.list_input').val('');
 
-      if (listName === 'blacklist' || listName === 'whitelist') {
-        programSpec[listName].push(nodeType);
+      if (listName === 'structure') {
+        // Let user choose where to add node
+        computeStructure();
       } else {
-        if (programSpec['structureList'].indexOf(nodeType) === -1) {
-          programSpec['structureList'].push(nodeType);
-        }
-        // Handle structure nodes
+        programSpec[listName].push(nodeType);
+        refreshNonLists();
+        refreshListUI();
+        refreshAutocompletes();
       }
-      programSpec['nonBlacklistNodes'] = [];
-      programSpec['nonWhiteBlacklistNodes'] = [];
-      programSpec['nonListNodes'] = [];
-      for (i = 0; i < nodeTypes.length; i++) {
-        if (programSpec['blacklist'].indexOf(nodeTypes[i]) === -1) {
-          programSpec['nonBlacklistNodes'].push(nodeTypes[i]);
-          if (programSpec['whitelist'].indexOf(nodeTypes[i]) === -1) {
-            programSpec['nonWhiteBlacklistNodes'].push(nodeTypes[i]);
-            if (programSpec['structureList'].indexOf(nodeTypes[i]) === -1) {
-              programSpec['nonListNodes'].push(nodeTypes[i]);
-            }
-          }
-        }
-      }
-
-      refreshLists();
-      refreshAutocompletes();
     };
   };
 
-  var refreshLists = function() {
+  var refreshNonLists = function() {
+    programSpec['nonBlacklistNodes'] = [];
+    programSpec['nonWhiteBlacklistNodes'] = [];
+    programSpec['nonListNodes'] = [];
+    for (i = 0; i < nodeTypes.length; i++) {
+      if (programSpec['blacklist'].indexOf(nodeTypes[i]) === -1) {
+        programSpec['nonBlacklistNodes'].push(nodeTypes[i]);
+        if (programSpec['whitelist'].indexOf(nodeTypes[i]) === -1) {
+          programSpec['nonWhiteBlacklistNodes'].push(nodeTypes[i]);
+          if (programSpec['structureList'].indexOf(nodeTypes[i]) === -1) {
+            programSpec['nonListNodes'].push(nodeTypes[i]);
+          }
+        }
+      }
+    }
+  };
+
+  var refreshListUI = function() {
     var i, nodeType;
     $('#whitelist_list').empty();
     $('#blacklist_list').empty();
     $('#structure_list').empty();
     for (i = 0; i < programSpec['whitelist'].length; i++) {
       nodeType = programSpec['whitelist'][i];
-      $('#whitelist_list').append('<div>' + nodeType + '</div>');
+      $('#whitelist_list').append(
+        '<div id="whitelist_' + nodeType + '" class="list_node">' +
+        nodeType + '<button class="close">&times</button></div>'
+      );
     }
     for (i = 0; i < programSpec['blacklist'].length; i++) {
       nodeType = programSpec['blacklist'][i];
-      $('#blacklist_list').append('<div>' + nodeType + '</div>');
+      $('#blacklist_list').append(
+        '<div id="blacklist_' + nodeType + '" class="list_node">' +
+        nodeType + '<button class="close">&times</button></div>'
+      );
     }
     for (i = 0; i < programSpec['structureList'].length; i++) {
       nodeType = programSpec['structureList'][i];
-      $('#structure_list').append('<div>' + nodeType + '</div>');
+      $('#structure_list').append(
+        '<div id="structure_' + nodeType + '" class="list_node">' +
+        nodeType + '<button class="close">&times</button></div>'
+      );
     }
   };
 
@@ -145,6 +162,29 @@ $(function() {
     });
   };
   refreshAutocompletes();
+
+  $('.node_list').on('click', '.close', function(e) {
+    var nodeDiv, splitId, listName, nodeType, idx;
+    nodeDiv = $(e.currentTarget).parent();
+    splitId = nodeDiv.attr('id').split('_');
+    listName = splitId[0];
+    nodeType = splitId[1];
+    console.log(listName, nodeType);
+
+    if (listName === "structure") {
+      nodeDiv.remove();
+      computeStructure();
+    } else {
+      idx = programSpec[listName].indexOf(nodeType);
+      if (idx !== -1) {
+        programSpec[listName].splice(idx, 1);
+      }
+      refreshListUI();
+    }
+
+    refreshNonLists();
+    refreshAutocompletes();
+  });
 
   $('#submit_code').click(function() {
     $.ajax({
